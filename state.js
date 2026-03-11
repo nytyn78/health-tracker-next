67379971d53120826776f980a31aa2a4c8aad4ff
nytyn78 <dr.nitinagarwal@gmail.com>
2026-03-11 22:41:18 +0530

    Update state.js

--- state.js
+++ state.js
@@ -1,9 +1,9 @@
 const saved = localStorage.getItem("healthTracker")
 
 export const state = saved
-  ? JSON.parse(saved)
-  : {
-      meals: [],
-      weights: [],
-      calorieTarget: 2000
-    }
+ ? JSON.parse(saved)
+ : {
+     meals: [],
+     weights: [],
+     calorieTarget: 2000
+   }
